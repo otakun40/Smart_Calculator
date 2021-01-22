6411318933c41acd04fcc22d63afda5008604900
otakun40 <otakun40@gmail.com>
2021-01-22 23:26:08 +0300

    Initial commit

---
+++ Smart Calculator/task/src/calculator/Main.java
@@ -0,0 +1,11 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(Integer.parseInt(scanner.next()) + Integer.parseInt(scanner.next()));
+    }
+}
